1a24af29b804f6702d9f10af2ccf93b90b8bc431
mdjawadulhasan <mdjawadulhasan@gmail.com>
2024-01-21 18:14:36 +0600

    Factory Design Pattern Added

---
+++ DotNetLearning/DesignPatterns/Factory/Factory.cs
@@ -0,0 +1,59 @@
+﻿public interface IVehicle
+{
+    void Drive();
+}
+
+public class Car : IVehicle
+{
+    public void Drive()
+    {
+        Console.WriteLine("Driving a car");
+    }
+}
+
+public class Bike : IVehicle
+{
+    public void Drive()
+    {
+        Console.WriteLine("Driving a Suzuki");
+    }
+}
+
+
+public interface IVehicleFactory
+{
+    IVehicle CreateVehicle();
+}
+
+
+public class CarFactory : IVehicleFactory
+{
+    public IVehicle CreateVehicle()
+    {
+        return new Car();
+    }
+}
+
+public class BikeFactory : IVehicleFactory
+{
+    public IVehicle CreateVehicle()
+    {
+        return new Bike();
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        IVehicleFactory carFactory = new CarFactory();
+        IVehicle car = carFactory.CreateVehicle();
+        car.Drive();
+
+
+        IVehicleFactory bikeFactory = new BikeFactory();
+        IVehicle bike = bikeFactory.CreateVehicle();
+        bike.Drive();
+    }
+}
